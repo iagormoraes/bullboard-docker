d304b6fc1f025480d5ca8d5b302e645900abe7e3
Iagor Moraes <iagor2014@outlook.com>
2023-06-01 13:47:21 -0300

    chore: setup bullboard docker config

---
+++ Dockerfile
@@ -0,0 +1,28 @@
+FROM node:alpine
+
+WORKDIR /usr/app
+
+COPY ./package.json .
+COPY ./yarn.lock .
+
+ENV NODE_ENV production
+ENV REDIS_HOST localhost
+ENV REDIS_PORT 6379
+ENV REDIS_USE_TLS false
+ENV REDIS_PASSWORD ''
+ENV BULL_PREFIX bull
+ENV BULL_VERSION BULLMQ
+ENV USER_LOGIN ''
+ENV USER_PASSWORD ''
+ENV REDIS_DB 0
+ENV PROXY_PATH ''
+
+RUN yarn install
+
+COPY . .
+
+ARG PORT=3000
+ENV PORT $PORT
+EXPOSE $PORT
+
+CMD ["node", "src/index.js"]
---
+++ src/index.js
@@ -0,0 +1,119 @@
+const { createBullBoard } = require('@bull-board/api')
+const { BullAdapter } = require('@bull-board/api/bullAdapter')
+const { BullMQAdapter } = require('@bull-board/api/bullMQAdapter')
+const { ExpressAdapter } = require('@bull-board/express')
+const Queue = require('bull')
+const bullmq = require('bullmq')
+const express = require('express')
+const redis = require('redis')
+const session = require('cookie-session')
+const passport = require('passport')
+const { ensureLoggedIn } = require('connect-ensure-login')
+const bodyParser = require('body-parser')
+
+const { authRouter } = require('./login')
+const config = require('./config')
+
+const redisConfig = {
+	redis: {
+		port: config.REDIS_PORT,
+		host: config.REDIS_HOST,
+		db: config.REDIS_DB,
+		...(config.REDIS_PASSWORD && { password: config.REDIS_PASSWORD }),
+		tls: config.REDIS_USE_TLS === 'true',
+	},
+}
+
+const serverAdapter = new ExpressAdapter()
+const { setQueues } = createBullBoard({ queues: [], serverAdapter })
+const router = serverAdapter.getRouter()
+
+const app = express()
+
+app.set('views', __dirname + '/views')
+app.set('view engine', 'ejs')
+
+if (app.get('env') !== 'production') {
+	const morgan = require('morgan')
+	app.use(morgan('combined'))
+}
+
+app.use((req, res, next) => {
+	if (config.PROXY_PATH) {
+		req.proxyUrl = config.PROXY_PATH
+	}
+
+	next()
+})
+
+const sessionOpts = {
+	name: 'bull-board.sid',
+	secret: Math.random().toString(),
+	resave: false,
+	saveUninitialized: false,
+	cookie: {
+		path: '/',
+		httpOnly: false,
+		secure: false,
+	},
+}
+
+app.use(session(sessionOpts))
+// register regenerate & save after the cookieSession middleware initialization
+app.use(function (request, response, next) {
+	if (request.session && !request.session.regenerate) {
+		request.session.regenerate = (cb) => {
+			cb()
+		}
+	}
+	if (request.session && !request.session.save) {
+		request.session.save = (cb) => {
+			cb()
+		}
+	}
+	next()
+})
+app.use(passport.initialize({}))
+app.use(passport.session({}))
+app.use(bodyParser.urlencoded({ extended: false }))
+
+if (config.AUTH_ENABLED) {
+	app.use(config.LOGIN_PAGE, authRouter)
+	app.use(config.HOME_PAGE, ensureLoggedIn(config.LOGIN_PAGE), router)
+} else {
+	app.use(config.HOME_PAGE, router)
+}
+
+app.listen(config.PORT, () => {
+	// eslint-disable-next-line no-console
+	console.log(`bull-board is started http://localhost:${config.PORT}${config.HOME_PAGE}`)
+	// eslint-disable-next-line no-console
+	console.log(`bull-board is fetching queue list, please wait...`)
+})
+
+async function run() {
+	const client = redis.createClient({ socket: redisConfig.redis })
+	await client.connect()
+	client.KEYS(`${config.BULL_PREFIX}:*`, (err, keys) => {
+		const uniqKeys = new Set(keys.map((key) => key.replace(/^.+?:(.+?):.+?$/, '$1')))
+		const queueList = Array.from(uniqKeys)
+			.sort()
+			.map((item) => {
+				if (config.BULL_VERSION === 'BULLMQ') {
+					const options = { redis: redisConfig.redis }
+					if (config.BULL_PREFIX) {
+						options.prefix = config.BULL_PREFIX
+					}
+					return new BullMQAdapter(new bullmq.Queue(item, options))
+				}
+
+				return new BullAdapter(new Queue(item, redisConfig))
+			})
+
+		setQueues(queueList)
+		// eslint-disable-next-line no-console
+		console.log('done!')
+	})
+}
+
+run()
